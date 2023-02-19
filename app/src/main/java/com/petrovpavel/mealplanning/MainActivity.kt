070f59f7aff9c6d57c3c2079541b1cbbf61b5fd5
Prioravod <Prioravod@github.com>
2023-02-20 03:46:14 +0400

    removed deprecated navigation mock

--- app/src/main/java/com/petrovpavel/mealplanning/MainActivity.kt
+++ app/src/main/java/com/petrovpavel/mealplanning/MainActivity.kt
@@ -3,11 +3,8 @@ package com.petrovpavel.mealplanning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.navigation.NavHostController
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.navigation.compose.rememberNavController
 import com.petrovpavel.mealplanning.ui.theme.MealPlanningTheme
 
@@ -21,26 +18,9 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = MaterialTheme.colorScheme.primary
                 ) {
-                    BottomNavigation(navController)
+                    BottomNavView(navController)
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun BottomNavigation(navHostController: NavHostController) {
-    var selectedItem by remember { mutableStateOf(0) }
-    val items = listOf("Receipts", "Meal plan", "Cart", "Account")
-
-    NavigationBar {
-        items.forEachIndexed { index, item ->
-            NavigationBarItem(
-                icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
-                label = { Text(item) },
-                selected = selectedItem == index,
-                onClick = { selectedItem = index }
-            )
         }
     }
 }
