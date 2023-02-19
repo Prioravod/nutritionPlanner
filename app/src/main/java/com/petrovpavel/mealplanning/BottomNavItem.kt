12962483c634a4f2b4ac5dc6f026114294baed21
Prioravod <Prioravod@github.com>
2023-02-20 03:37:09 +0400

    added sealed class for NavigationBar items links storage

---
+++ app/src/main/java/com/petrovpavel/mealplanning/BottomNavItem.kt
@@ -0,0 +1,9 @@
+package com.petrovpavel.mealplanning
+
+sealed class BottomNavItem(var title:String, var icon:Int, var screen_route:String){
+
+    object Recipes : BottomNavItem("Recipes", R.drawable.outline_trending_up_24,"recipes")
+    object MealPlan: BottomNavItem("Meal plan", R.drawable.outline_calendar_month_24,"meal_plan")
+    object Cart: BottomNavItem("Cart", R.drawable.outline_shopping_cart_24,"cart")
+    object Account: BottomNavItem("Account", R.drawable.outline_account_circle_24,"account")
+}
